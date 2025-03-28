4aa0494ce5a3b1d7a1ae2ac58909417d5f0e6404
GabrielaRBLemos <gabrielarblemos@gmail.com>
2025-03-28 10:13:35 -0300

    typing animation

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,32 @@
-console.log("fghjk");
-//font change
-//document.getElementsByClassName("p").style.fontFamily = "serif"
+// START efeito de digitação
+const logo = document.getElementById("logo");
+const text = logo.textContent;
+const defaultText = " ";
+let index = 0;
+
+logo.textContent = "";
+
+function type() {
+    if (index < text.length) {
+        logo.textContent += text.charAt(index);
+        index++;
+        setTimeout(type, 100);
+    } else {
+        setTimeout(remove, 1000);
+    }
+}
+
+function remove() {
+    if (index > 0) {
+        logo.textContent = logo.textContent.slice(0, -1);
+        index--;
+        setTimeout(remove, 100);
+    } else {
+        logo.textContent = defaultText;
+        setTimeout(type, 1000);
+    }
+}
+type();
+// END efeito de digitação
+console.log(logo.textContent);
+
