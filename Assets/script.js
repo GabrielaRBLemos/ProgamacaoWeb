c826cc646d4faa27f6de2858734c36dff0909b50
GabrielaRBLemos <gabrielarblemos@gmail.com>
2025-03-29 19:05:20 -0300

    jogo da senha

--- Assets/script.js
+++ Assets/script.js
@@ -50,5 +50,62 @@ if (window.location.href == window.location.origin + "/portfolio/index.html") {
         });
     };
 }
-
-
+if (window.location.href == window.location.origin + "/portfolio/senha.html") {
+    function getRandomCode() {
+        let digits = [];
+        while (digits.length < 4) {
+            let digit = Math.floor(Math.random() * 10);
+            if (!digits.includes(digit)) {
+                digits.push(digit);
+            }
+        }
+        return digits.join('');
+    }
+    const code = getRandomCode();
+    alert("A senha é: "+code)
+    function mixedExactCount(codeArray, answerArray) {
+        let exactNum = 0;
+        let mixedNum = 0;
+        //exacts
+        for (let i = 0; i < 4; i++) {
+            if (codeArray[i] === answerArray[i]) {
+                exactNum++;
+                // marcando posições como nulas pra não contá-las como misturadas
+                codeArray[i] = null;
+                answerArray[i] = null;
+            }
+        }
+        //mixed
+        for (let i = 0; i < 4; i++) {
+            if (answerArray[i] !== null && codeArray.includes(answerArray[i])) {
+                mixedNum++;
+                codeArray[codeArray.indexOf(answerArray[i])] = null; // pra evitar contar de novo
+            }
+        }
+        return { exactNum, mixedNum };
+    }
+    function createNewItem(codeAnswer,exactNum,mixedNum) {
+        const newAttempt = document.createElement('li');
+        const attemptList = document.getElementById('attemptList');
+        newAttempt.innerHTML = codeAnswer + ' - <img src="/Assets/droplet.svg"/>: ' + exactNum + ' <img src="/Assets/droplet-half-2.svg"/>: ' + mixedNum;
+        
+        if (attemptList.firstChild) {
+            attemptList.insertBefore(newAttempt, attemptList.firstChild);
+        } else {
+            attemptList.appendChild(newAttempt); // If the list is empty, just append
+        }
+    }
+    function checkCode() {
+        let codeAnswer = document.getElementById("code").value;
+        let codeArray = code.split('');
+        let answerArray = codeAnswer.split('');
+        const { exactNum, mixedNum } = mixedExactCount(codeArray, answerArray);
+        if (exactNum == 4) {
+            document.getElementById("result").innerText = "Parabéns! Você acertou!";
+            document.getElementById("codeButton").innerText = "Jogar de novo?";
+            document.getElementById("code").value = "";
+            document.getElementById("codeButton").onclick = function() {location.reload();};
+        }
+        createNewItem(codeAnswer, exactNum, mixedNum);
+    }
+}
