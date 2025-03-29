da7d0f6b83f7791004654ac93eb83264bb98b069
GabrielaRBLemos <gabrielarblemos@gmail.com>
2025-03-29 16:00:14 -0300

    portfolio hor scroll

--- Assets/script.js
+++ Assets/script.js
@@ -1,32 +1,54 @@
-// START efeito de digitação
-const logo = document.getElementById("logo");
-const text = logo.textContent;
-const defaultText = " ";
-let index = 0;
+const host = window.location.origin
+if (window.location.href == window.location.origin + "/index.html" || window.location.href == window.location.origin + "/portfolio/index.html") {
+    // START efeito de digitação
+    const typed = document.getElementById("typed");
+    const text = typed.textContent;
+    const defaultText = " ";
+    let index = 0;
 
-logo.textContent = "";
+    typed.textContent = "";
 
-function type() {
-    if (index < text.length) {
-        logo.textContent += text.charAt(index);
-        index++;
-        setTimeout(type, 100);
-    } else {
-        setTimeout(remove, 1000);
+    function type() {
+        if (index < text.length) {
+            typed.textContent += text.charAt(index);
+            index++;
+            setTimeout(type, 100);
+        } else {
+            setTimeout(remove, 1000);
+        }
     }
+
+    function remove() {
+        if (index > 0) {
+            typed.textContent = typed.textContent.slice(0, -1);
+            index--;
+            setTimeout(remove, 100);
+        } else {
+            typed.textContent = defaultText;
+            setTimeout(type, 1000);
+        }
+    }
+    type();
+    // END efeito de digitação
+}
+if (window.location.href == window.location.origin + "/portfolio/index.html") {
+    //START Portfolio buttons
+    const portfolio = document.getElementById("port");
+    const itemWidth = portfolio.offsetWidth;
+    const rightButton = document.getElementById("right-button");
+    const leftButton = document.getElementById("left-button");
+    rightButton.onclick = () => {
+        portfolio.scrollBy({
+            left: itemWidth / 4,
+            behavior: 'smooth' // Adiciona o scroll suave
+        });
+    }
+    leftButton.onclick = () => {
+        portfolio.scrollBy({
+            left: -(itemWidth / 4),
+            behavior: 'smooth'
+        });
+    };
 }
 
-function remove() {
-    if (index > 0) {
-        logo.textContent = logo.textContent.slice(0, -1);
-        index--;
-        setTimeout(remove, 100);
-    } else {
-        logo.textContent = defaultText;
-        setTimeout(type, 1000);
-    }
-}
-type();
-// END efeito de digitação
-console.log(logo.textContent);
 
